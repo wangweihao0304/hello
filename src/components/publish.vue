45548296dfcb6f905be0fe1549de573176e1f2bb
wangweihao0304 <1802676721@qq.com>
2025-04-30 10:55:05 +0800

    weibo

--- src/components/publish.vue
+++ src/components/publish.vue
@@ -1,57 +1,60 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-let content = ref('');
+let content = ref('')
+
 let publish = () => {
   console.log(content.value)
   content.value = ''
 }
-
 </script>
 
 <template>
- <div class="input-wrapper">
-    <input type="text" placeholder="请输入内容" v-model="content" class="input-content" @keyup.enter="publish">
+  <div class="input-wrapper">
+    <input type="text" placeholder="请输入内容" v-model="content" class="input-content" @keyup.enter="publish" />
     <button @click="publish" class="button-input">发布</button>
- </div>
+  </div>
 </template>
-
 
 <style scoped>
 .input-wrapper {
-    height: 50px;
-    margin-top: 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
+  height: 50px;
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
 }
+
 .input-content {
-    flex: 1;
-    height:50px;
-    border-radius: 5px;
-    border: 1px solid ;
-    margin-right: 20px;
+  flex: 1;
+  height: 50px;
+  border-radius: 5px;
+  border: 1px solid;
+  margin-right: 20px;
 }
-/*输入框.input-content输入内容时外框变成黄色 */
-.input-content:focus{
-    border: 1px solid orange;
-    outline: none;
+
+/* 输入框.input-content输入内容时外框变成黄色 */
+.input-content:focus {
+  border: 1px solid rgb(111, 150, 201);
+  outline: none;
 }
-.button-input{
-    width: 100px;
-    height: 50px;
-    border-radius: 5px;
-    background-color: orange;
-    color: #fff;
-    border: none;
+
+.button-input {
+  width: 100px;
+  height: 50px;
+  border-radius: 5px;
+  background-color: orange;
+  color: #fff;
+  border: none;
 }
-.button-input:hover{
-    background-color: rgb(255, 165, 0);
-    cursor: pointer;
-    color: coral;
-    transition: 0.5s;
+
+.button-input:hover {
+  background-color: rgb(255, 165, 0);
+  cursor: pointer;
+  color: coral;
+  transition: 0.5s;
 }
 </style>
