60e1ceb7179ae2a9cf61ab482d73f5fc27b565f7
wangweihao0304 <1802676721@qq.com>
2025-04-30 16:25:24 +0800

    weibo

--- src/components/head.vue
+++ src/components/head.vue
@@ -30,7 +30,7 @@ const login = () => {
 .guide-head {
   width: 100%; 
   height: 75px;
-  background-color: #ffa500;
+  background-color: #fff5e3;
   display: flex;
   justify-content: space-between; 
   align-items: center;
@@ -39,7 +39,9 @@ const login = () => {
 }
 .guide-head-title {
   margin-left: 40px;
-  color: yellow;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgb(39, 39, 33);
 }
 .login {
   width: 80px;
@@ -48,11 +50,14 @@ const login = () => {
   color: #fff;
   background-color: #ffa500;
   border-radius: 10px;
-  box-shadow: 0 0 10px #ffa500;
+  box-shadow: 0 0 10px #bc7a00;
   border: none;
+  transition: background-color 0.3s ,transform 0.3s;
 }
 .login:hover {
-  color: #ffa500;
-  background-color: #fff;
+  cursor: pointer;
+  
+  background-color: #627bb0;
+  transform: scale(1.1);
 }
 </style>
--- src/components/post.vue
+++ src/components/post.vue
@@ -3,7 +3,7 @@
         <div class="comment-item">
             <h2>{{ item.title }}</h2>
             <div class="comment-author">
-                {{ item.userid}}
+                {{ item.username}}
                 <img :src="item.imageurl">
             </div>
             <div class="comment-content">{{ item.content }}</div>
@@ -12,7 +12,7 @@
                 <RouterLink to="/likes" class="likes">喜欢:{{ item.likes }}</RouterLink>
                 <RouterLink to="/comments" class="comments">评论:{{ item.comments }}</RouterLink>
             </div>
-            <button type="submit" :value="item.postid" @click="detail">详情</button>
+            <button type="submit" :value="item.postid" @click="detail" class="details">详情</button>
         </div>
     </div>
 </template>
@@ -36,9 +36,27 @@
         display: flex;
         justify-content:space-between;
         flex-direction: column; 
-        color: #fff;
-        background-color: orange;
+        color: #1d1c1c;
+        background-color: rgb(228, 228, 228);
     }
+
+    .details {
+  width: 80px;
+  height: 20px;
+  margin-right: 40px;
+  color: #fff;
+  background-color: #747069;
+  border-radius: 10px;
+  box-shadow: 0 0 10px #656463;
+  border: none;
+  transition: background-color 0.3s ,transform 0.3s;
+}
+.details:hover {
+  cursor: pointer;
+  
+  background-color: #657eb4;
+  transform: scale(1.1);
+}
     .comment-info{
         display: flex;
         justify-content: space-between;
@@ -53,22 +71,25 @@
         border-radius: 50%;
     }
     .comment-author{
+        color: #171717;
         display: flex;
         align-items: center;
     }
     .publishtimecss{
-        color: #999;
+        color: #2c2b2b;
         font-size: 12px;
         margin: 10px;
     }
     .likes{
-        color: red;
+        color: rgb(52, 48, 48);
         font-size: 18px;
+        font-weight: bold;
 
     }
     .comments{
-        color: #999;
+        color: #333131;
         font-size: 18px;
+        font-weight: bold;
         margin-right: 10px;
     }
     .comment-content{
